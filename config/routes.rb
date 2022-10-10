fa5ba1dbd70afc2a894a50026195be057e445af2
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-10-10 14:40:49 +0000

    devise機能編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  
+# 団体用
+# URL /teams/sign_in ...
+devise_for :teams, skip:[:passwords],controllers: {
+  registrations: "group/registrations",
+  sessions: 'group/sessions'
+}
+  
+# 顧客用
+# URL /persons/sign_in ...
+devise_for :people, skip:[:passwords], controllers: {
+  registrations: "people/registrations",
+  sessions: 'people/sessions'
+}
+  
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
