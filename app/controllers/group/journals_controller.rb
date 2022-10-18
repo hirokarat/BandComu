9a6daa58979b00c3465567db211a39609ae1dc99
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-10-18 13:28:49 +0000

    投稿機能作成1

--- app/controllers/group/journals_controller.rb
+++ app/controllers/group/journals_controller.rb
@@ -1,2 +1,31 @@
 class Group::JournalsController < ApplicationController
+  
+  def new
+    @journal=Journal.new
+  end
+  
+  def create
+    @journal=Journal.new(journal_params)
+    if @journal.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to index
+    else
+      render new
+    end
+  end
+  
+  def index
+    @journals=Journal.all
+  end
+  
+  def destroy
+    
+  end
+  
+  private
+  def journal_params
+    params.require(:journal).permit(:journal, :activity_image)  
+  end
+  
+  
 end
