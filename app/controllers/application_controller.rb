0f5c96968889d58fd6109377cce89045bbe27bee
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-10-18 14:08:12 +0000

    teamの会員機能作成1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,37 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action:configure_permitted_parameters,if: :devise_controller?
+
+  def after_sign_in_path_for(resource_or_scope)
+    if resource_or_scope.is_a?(Admin)
+      admin_root_path
+    else
+      root_path
+    end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
+  end
+
+  def user_shut_out
+    unless admin_signed_in?
+      redirect_to root_path
+    end
+  end
+  
+  def after_sign_up_path_for(resource_or_scope)
+    my_page_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:email])
+  end
+
 end
