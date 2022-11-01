8072cc8f7589f7b6ddb76000137d1ea73f14dc4f
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-11-01 13:49:03 +0000

    タグ機能調整

--- app/controllers/group/journals_controller.rb
+++ app/controllers/group/journals_controller.rb
@@ -1,44 +1,64 @@
 class Group::JournalsController < ApplicationController
-  
+
+  before_action :authenticate_team!
+
+
   def new
     @journal=Journal.new
   end
-  
+
   def create
     @journal=Journal.new(journal_params)
     @journal.team_id=current_team.id
     # 受け取った値を,で区切って配列にする
     tag_list=params[:journal][:name].split(',')
     if @journal.save
-       @journal.save_tag(tag_list)
-      redirect_to group_journals_path(@journal),notice:'投稿完了しました:)'
+       @journal.save_journals(tag_list)
+      redirect_to group_journals_path,notice:'投稿完了しました'
     else
+      flash.now[:alert] = '投稿に失敗しました'
       render new
     end
   end
-  
+
   def index
     @journals = Journal.page(params[:page]).per(10)
+    @tag_lists=Tag.all
+    if params[:tag_id].present?
+      @tag = Tag.find(params[:tag_id])
+      @journals = @tag.journals.order(created_at: :desc)
+    else
+      @journals = Journal.all.order(created_at: :desc)
+    end
+  end
+
+  def search_tag
+    #検索結果画面でもタグ一覧表示
     @tag_list=Tag.all
-    @journals = params[:tag_id].present? ? Tag.find(params[:tag_id]).journals : Journal.all
+    #検索されたタグを受け取る
+    @tag=Tag.find(params[:tag_id])
+    #検索されたタグに紐づく投稿を表示
+    @journals=@tag.journals.page(params[:page]).per(10)
   end
-  
-  
+
+
   def destroy
-    
+    journal = Journal.find(params[:id])
+    journal.destroy
+    redirect_to group_journals_path
   end
-  
+
   def show
     @journal = Journal.find(params[:id])
-    @journal_comment=JournalComment.new
+
     @journal_tags = @journal.tags
   end
-  
+
   def edit
     @journal = Journal.find(params[:id])
     @tag_list=@journal.tags.pluck(:name).join(',')
   end
-  
+
   def update
     @journal = Journal.find(params[:id])
     tag_list=params[:journal][:name].split(',')
@@ -49,11 +69,11 @@ class Group::JournalsController < ApplicationController
       render:edit
     end
   end
-  
+
   private
   def journal_params
-    params.require(:journal).permit(:journal,:team_id,:activity_image,tag_ids: [])  
+    params.require(:journal).permit(:journal,:activity_image)
   end
-  
-  
+
+
 end
