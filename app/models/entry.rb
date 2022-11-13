04d5a9b445a849380d55bf6d55be13d356e7c394
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2022-11-13 14:42:17 +0000

    レイアウト調整

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,9 @@ class Entry < ApplicationRecord
   belongs_to:person
   belongs_to:team
   validates :person_id, uniqueness: { scope: :team_id }
+  
+  def full_name
+    self.person.last_name + self.person.first_name
+  end
+  
 end
