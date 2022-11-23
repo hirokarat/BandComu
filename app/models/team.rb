6614b3d1f6a8e7e20375fe1c02317747e797f9ce
hirokarat <hirotaka.e1111@gmail.com>
2022-11-23 14:10:25 +0000

    レイアウト編集

--- app/models/team.rb
+++ app/models/team.rb
@@ -17,7 +17,15 @@ class Team < ApplicationRecord
       end
     header_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
+  def get_profile_image(width, height)
+      unless profile_image.attached?
+        file_path = Rails.root.join('app/assets/images/no_image.png')
+        profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+
   def self.guest
     find_or_create_by!(email: 'aaa@aaa.com') do |team|
       team.password = SecureRandom.urlsafe_base64
